7866c978653b70809ee7a2282231ee782d48cf19
iMohsen <iMohsen2002@gmail.com>
2023-04-11 23:01:40 +0330

    Fetcher added.

---
+++ src/main/java/Fetcher.java
@@ -0,0 +1,14 @@
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class Fetcher {
+    public WebPage fetch(@NotNull WebPage webPage) throws IOException {
+
+        webPage.setContent(
+                Jsoup.connect(webPage.getUrl().toString()).get().toString()
+        );
+        return webPage;
+    }
+}
