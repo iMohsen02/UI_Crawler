ae162686e958e21a30b82d6a04ac7d9c48acf34b
iMohsen <iMohsen2002@gmail.com>
2023-04-13 21:16:46 +0330

    first demo

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,38 +1,40 @@
 import java.io.IOException;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.List;
 
 public class Crawler {
-    private Fetcher fetcher;
-    private Parser parser;
-    private UniquenessChecker uniquenessChecker;
-    private Repository repository;
-    private URLs urls;
-    private int maxPage;
+    private final Fetcher fetcher;
+    private final Parser parser;
+    private final UniquenessChecker uniquenessChecker;
+    private final Repository repository;
+    private final URLs urls;
+    private final int maxPage;
 
     public Crawler(URL seed, int maxPage) throws IOException {
         this.fetcher = new Fetcher();
         this.parser = new Parser();
         this.uniquenessChecker = new UniquenessChecker();
-        Repository.init("src/main/resources/pages.txt");
+        Repository.init("src/main/resources/pages.xml");
         this.repository = Repository.getInstance();
         this.urls = new URLs(seed);
         this.maxPage = maxPage;
     }
 
-    public void crawl() throws IOException {
+    public void crawl() throws IOException, ContentNotFetchYet {
         for (int i = 0; i < maxPage; i++) {
-            WebPage webpage;
+            WebPage webpage = new WebPage(urls.getNextURL());
             try {
-                webpage = fetcher.fetch(new WebPage(urls.getNextURL()));
+                webpage = fetcher.fetch(webpage);
+                System.out.println(webpage.getUrl());
             } catch (Exception exception) {
-                System.out.println("can't fetch.");
+                System.out.println("can't fetch. => " );
                 i--;
                 continue;
             }
             List<URL> subUrls = parser.parse(webpage);
-            repository.saveContent(webpage.getContent());
+            subUrls.forEach(System.out::println);
+
+            repository.saveContent(webpage.generateFileTemplate());
 
             subUrls
                     .stream()
@@ -41,5 +43,6 @@ public class Crawler {
 
 
         }
+        repository.closeRepository();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
+import java.net.URL;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ContentNotFetchYet {
+
+        Crawler crawler = new Crawler(new URL("https://yazd.ac.ir/"), 2);
+        crawler.crawl();
 
     }
 }
