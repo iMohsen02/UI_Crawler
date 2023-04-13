c795b76e51ca7b463b95c3863bde5c4b4476aa30
iMohsen <iMohsen2002@gmail.com>
2023-04-13 21:13:39 +0330

    fetch document instead of text

--- src/main/java/Fetcher.java
+++ src/main/java/Fetcher.java
@@ -7,7 +7,7 @@ public class Fetcher {
     public WebPage fetch(@NotNull WebPage webPage) throws IOException {
 
         webPage.setContent(
-                Jsoup.connect(webPage.getUrl().toString()).get().toString()
+                Jsoup.connect(webPage.getUrl().toString()).get()
         );
         return webPage;
     }
