9853955f605d3bc9114bf9b7919b73635ee0eaea
iMohsen <iMohsen2002@gmail.com>
2023-04-18 17:16:02 +0330

    urlBlocker init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 import java.io.IOException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ContentNotFetchYet {
+    public static void main(String[] args) throws IOException, ContentNotFetchYet, InterruptedException {
 
-        Crawler crawler = new Crawler(new URL("https://yazd.ac.ir/"), 2);
+        long first = System.currentTimeMillis();
+        UrlBlocker filtered = new UrlBlocker();
+        filtered.addBlock(
+                "https://www.facebook",
+                "https://facebook",
+                "https://telegram",
+                "https://www.telegram",
+                "https://instagram",
+                "https://www.instagram",
+                "https://twitter",
+                "https://www.twitter",
+                "https://t.me",
+                "https://youtube.me",
+                "https://www.youtube.me"
+        );
+
+        Crawler crawler = new Crawler(new URL("https://yazd.ac.ir/"), 12000, filtered);
         crawler.crawl();
+
+        new Scanner(System.in).nextLine();
+//        System.out.println((System.currentTimeMillis() - first) / 1000.0);
 
     }
 }
