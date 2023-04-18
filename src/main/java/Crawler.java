323227ed21f7b375d385f8dbc2df0681588c9661
iMohsen <iMohsen2002@gmail.com>
2023-04-18 17:15:45 +0330

    run in async mode.

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,48 +1,58 @@
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class Crawler {
     private final Fetcher fetcher;
     private final Parser parser;
-    private final UniquenessChecker uniquenessChecker;
     private final Repository repository;
     private final URLs urls;
     private final int maxPage;
+    private final Report report;
 
-    public Crawler(URL seed, int maxPage) throws IOException {
-        this.fetcher = new Fetcher();
+    public Crawler(URL seed, int maxPage, UrlBlocker urlBlocker) throws IOException {
+        this.fetcher = new Fetcher(urlBlocker);
         this.parser = new Parser();
-        this.uniquenessChecker = new UniquenessChecker();
         Repository.init("src/main/resources/pages.xml");
         this.repository = Repository.getInstance();
         this.urls = new URLs(seed);
         this.maxPage = maxPage;
+        this.report = new Report();
     }
 
-    public void crawl() throws IOException, ContentNotFetchYet {
+    public void crawl() throws ContentNotFetchYet {
+
+        List<CompletableFuture<Void>> completableFutureList = new ArrayList<>();
         for (int i = 0; i < maxPage; i++) {
-            WebPage webpage = new WebPage(urls.getNextURL());
-            try {
-                webpage = fetcher.fetch(webpage);
-                System.out.println(webpage.getUrl());
-            } catch (Exception exception) {
-                System.out.println("can't fetch. => " );
-                i--;
-                continue;
-            }
-            List<URL> subUrls = parser.parse(webpage);
-            subUrls.forEach(System.out::println);
+            System.out.println(i);
+            final WebPage webpage = new WebPage(urls.getNextURL());
 
-            repository.saveContent(webpage.generateFileTemplate());
-
-            subUrls
-                    .stream()
-                    .filter(uniquenessChecker::check)
-                    .forEach(urls::addURL);
+            completableFutureList.add(
+                    fetcher.fetchAsync(webpage)
+                            .thenComposeAsync(parser::parseAsync)
+                            .thenAcceptAsync(urls::addUrlAsync)
+                            .thenRunAsync(() -> {
+                                try {
+                                    repository.saveContent(webpage.generateFileTemplate());
+                                } catch (IOException e) {
+                                    System.out.println("Crawler can't continue because IO exception accrued. can't write content");
+                                    throw new RuntimeException(e);
+                                }
+                            }).thenRunAsync(() -> report.addReport(webpage.getStatus()))
+            );
 
 
         }
-        repository.closeRepository();
+        CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[0])).thenRun(() -> {
+            try {
+                repository.closeRepository();
+                System.out.println(report);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
     }
 }
