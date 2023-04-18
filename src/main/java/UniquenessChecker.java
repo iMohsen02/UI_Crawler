d04a72226bb1b1d7a8601b288a20d1f0c24c7d3d
iMohsen <iMohsen2002@gmail.com>
2023-04-18 17:14:45 +0330

    data structure changed

--- src/main/java/UniquenessChecker.java
+++ src/main/java/UniquenessChecker.java
@@ -5,9 +5,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 public class UniquenessChecker {
-    private final Set<String> checked = new HashSet<>();
+    private final Set<String> checked = new ConcurrentSkipListSet<>();
 
     public boolean check(final @NotNull URL url) {
         return this.checked.add(url.getHost());
