839b854b1fcf2596d506a2b7a3fbc1fa23cc9609
iMohsen <iMohsen2002@gmail.com>
2023-04-18 17:12:22 +0330

    run in async mode added.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,11 +1,12 @@
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jsoup.Jsoup;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class Parser {
     private final static String ignore = "txt, pdf, tif, tiff, bmp, jpg, jpeg, gif, png, esp, docs, xlsx, doc, pptm, xls, xlsm".trim().replace(", ", "|");
@@ -18,13 +19,23 @@ public class Parser {
         }
     }
 
-    public List<URL> parse(final @NotNull WebPage page) throws ContentNotFetchYet {
-        return page.getContent()
-                .select("a[href~=.*(?<!" + ignore + ")$]")
-                .stream()
-                .map(link -> Parser.apply(link.attr("href")))
-                .filter(Objects::nonNull)
-                .toList();
+    public List<URL> parse(final @NotNull WebPage page) {
 
+//        long time = System.currentTimeMillis();
+        List<URL> hrefs = page.getStatus() != WebPage.Status.CRAWLED ? new ArrayList<>() :
+                page.getContent()
+                        .select("a[href~=.*(?<!" + ignore + ")$]")
+                        .stream()
+                        .map(link -> Parser.apply(link.attr("href")))
+                        .filter(Objects::nonNull)
+                        .toList();
+
+//        System.out.println("\tparse:\t" + (System.currentTimeMillis() - time) / 1000.);
+//        System.out.println("\t\t" + page);
+        return hrefs;
+    }
+
+    public CompletableFuture<List<URL>> parseAsync(final @NotNull WebPage page) {
+        return CompletableFuture.supplyAsync(() -> this.parse(page));
     }
 }
